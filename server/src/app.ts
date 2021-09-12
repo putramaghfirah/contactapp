386f00614c0e586909aa872bdf8c77d8c7c3fbe0
putramaghfirah <putra.maghfirah@gmail.com>
2021-09-12 09:28:50 +0700

    ✨ chore: database connection

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,8 @@
+// library
 import express, { Response, Request } from 'express'
-const cors = require('cors')
+const cors = require('cors') // cors to remove policy fetch
+const mongoose = require('mongoose') // mongoose
+require('dotenv/config') // dotenv to catch DB_CONNECTION
 
 const app = express()
 const PORT = 4000
@@ -15,8 +18,14 @@ app.get('/', (req: Request, res: Response) => {
   res.json(mhs)
 })
 
+// page not found
 app.use('/', (req: Request, res: Response) => {
   res.status(404).send('404 Page not found :(')
+})
+
+// database connect
+mongoose.connect(process.env.DB_CONNECTION, () => {
+  console.log('connected to DB!')
 })
 
 app.listen(PORT, () => {
