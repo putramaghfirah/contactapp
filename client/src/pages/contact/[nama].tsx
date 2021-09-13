9c8acc8376edd268b388486495a268fa4657d078
putramaghfirah <putra.maghfirah@gmail.com>
2021-09-13 08:15:37 +0700

    💄 style: add ui detail contact

--- client/src/pages/contact/[nama].tsx
+++ client/src/pages/contact/[nama].tsx
@@ -2,6 +2,7 @@ import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 import { Contact } from '../../types/Contact'
+import { ArrowLeftIcon } from '@heroicons/react/solid'
 
 interface Props {
   contact: Contact[]
@@ -13,23 +14,46 @@ const DetailContact = ({ contact }: Props): JSX.Element => {
       <Head>
         <title>Detail Page</title>
       </Head>
-      {contact.map(contact => {
-        return (
-          <div
-            key={contact.email}
-            className="text-3xl font-bold  dark:text-gray-100"
-          >
-            {contact.email}
-            {contact.nama}
-            {contact.nohp}
-          </div>
-        )
-      })}
-      <Link href="/" passHref>
-        <a className="font-semibold text-blue-600 hover:text-blue-700">
-          Go back
-        </a>
-      </Link>
+      <div className="flex items-center justify-center min-h-screen">
+        {contact.map(contact => {
+          return (
+            <div
+              key={contact.email}
+              className="max-w-2xl px-8 py-4 mx-auto bg-white rounded-lg shadow-md dark:bg-gray-800"
+            >
+              <div className="mt-2">
+                <p className="text-2xl font-bold text-gray-700 dark:text-white hover:text-gray-600 dark:hover:text-gray-200">
+                  {contact.nama}
+                </p>
+                <p className="mt-2 text-gray-600 dark:text-gray-300">
+                  {contact.email}
+                </p>
+                <p className="mt-2 text-gray-600 dark:text-gray-300">
+                  {contact.nohp}
+                </p>
+              </div>
+
+              <div className="flex items-center justify-between mt-2">
+                <a className="px-3 py-1 text-sm font-bold text-gray-100 transition-colors duration-200 transform bg-blue-600 rounded cursor-pointer hover:bg-blue-500">
+                  Edit
+                </a>
+                <a className="px-3 py-1 text-sm font-bold text-gray-100 transition-colors duration-200 transform bg-red-600 rounded cursor-pointer hover:bg-red-500">
+                  Hapus
+                </a>
+              </div>
+
+              <div className="flex items-center mt-4">
+                <Link href="/" passHref>
+                  <a className="text-blue-600 dark:text-blue-400 hover:underline flex items-center">
+                    <ArrowLeftIcon className="w-4 h-4 mr-1" />
+                    Go Back
+                  </a>
+                </Link>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </>
   )
 }
