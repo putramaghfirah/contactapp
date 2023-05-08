d6d06bff1309da6f25bbe6eadf6b5f0d70e9193d
putramaghfirah <putra.maghfirah@gmail.com>
2023-05-09 00:12:57 +0700

    feat: :sparkles: add server action and update button

--- client-new/src/components/Button.tsx
+++ client-new/src/components/Button.tsx
@@ -1,11 +1,10 @@
 import { cva, VariantProps } from 'class-variance-authority'
 import clsx from 'clsx'
-import { ReactNode } from 'react'
 
 import { ButtonOrLink, Props as ButtonOrLinkProps } from './ButtonOrLink'
 
 const buttonStyles = cva(
-  'inline-flex items-center rounded border border-transparent font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 dark:focus:ring-offset-0',
+  'inline-flex items-center justify-center rounded border border-transparent font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 dark:focus:ring-offset-0 disabled:opacity-60 disabled:pointer-events-none hover:bg-opacity-80',
   {
     variants: {
       intent: {
@@ -19,40 +18,36 @@ const buttonStyles = cva(
         medium: 'px-4 py-2 text-sm',
         large: 'px-4 py-2 text-base',
       },
+      fullWidth: {
+        true: 'w-full',
+      },
     },
     defaultVariants: {
       intent: 'primary',
       size: 'medium',
+      fullWidth: false,
     },
   },
 )
 
-interface Props extends VariantProps<typeof buttonStyles>, ButtonOrLinkProps {
+export interface Props
+  extends VariantProps<typeof buttonStyles>,
+    ButtonOrLinkProps {
   className?: string
-  isLoading?: boolean
-  children?: ReactNode
 }
 
 export default function Button(props: Props) {
   const {
     intent = 'primary',
     size = 'medium',
+    fullWidth,
     className,
-    isLoading,
-    children,
     ...rest
   } = props
   return (
     <ButtonOrLink
-      className={clsx(
-        buttonStyles({ intent, size }),
-        className,
-        isLoading && 'cursor-not-allowed opacity-75',
-      )}
-      disabled={isLoading}
+      className={clsx(buttonStyles({ intent, size, fullWidth }), className)}
       {...rest}
-    >
-      {isLoading ? 'Loading...' : children}
-    </ButtonOrLink>
+    />
   )
 }
