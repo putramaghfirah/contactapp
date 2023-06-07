e81ca0394832f38ce7244cac42a11c47031361ce
putramaghfirah <putra.maghfirah@gmail.com>
2023-06-07 13:10:53 +0700

    chore: :sparkles: update lib

--- client-new/src/lib/getAllContacts.ts
+++ client-new/src/lib/getAllContacts.ts
@@ -1,6 +1,6 @@
 export default async function getAllContacts() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/contact`, {
-    next: { revalidate: 1 },
+    cache: 'no-store',
   })
 
   if (!res.ok) throw new Error('failed to fetch user')
--- client-new/src/lib/getContact.ts
+++ client-new/src/lib/getContact.ts
@@ -1,6 +1,7 @@
 export default async function getContact(contactId: string) {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_APP_URL}/contact/${contactId}`,
+    { cache: 'no-store' },
   )
 
   if (!res.ok) return undefined
